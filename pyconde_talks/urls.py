0813803586858e29b12c4588831f2394498f9200
Julio Batista Silva <julio@juliobs.com>
2025-02-18 00:59:08 +0100

    Update urls.py: allauth use accounts/ path

--- pyconde_talks/urls.py
+++ pyconde_talks/urls.py
@@ -1,9 +1,10 @@
 """URL configuration for pyconde_talks project."""
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("accounts/", include("allauth.urls")),
 ]
