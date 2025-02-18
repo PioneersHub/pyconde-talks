583e16a9565cde58c61b69b923205971e92103cd
Julio Batista Silva <julio@juliobs.com>
2025-02-18 04:16:01 +0100

    users/views.py: HomeView

--- users/views.py
+++ users/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.utils import timezone
+from django.views.generic import TemplateView
 
-# Create your views here.
+from .models import Talk
+
+
+class HomeView(TemplateView):
+    template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.request.user.is_authenticated:
+            today = timezone.now().date()
+            context.update(
+                {
+                    "total_talks": Talk.objects.count(),
+                    "todays_talks": Talk.objects.filter(date_time__date=today).count(),
+                    "recorded_talks": Talk.objects.exclude(video_link="").count(),
+                    "upcoming_talks": Talk.objects.filter(
+                        date_time__gte=timezone.now(),
+                    ).order_by("date_time")[:5],
+                }
+            )
+        return context
