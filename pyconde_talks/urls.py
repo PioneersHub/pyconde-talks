0f0165beebe51044a8000c4d1975b8aa2719787a
Julio Batista Silva <julio@juliobs.com>
2025-02-18 03:38:57 +0100

    settings.py, urls.py: configure static and media directories

--- pyconde_talks/urls.py
+++ pyconde_talks/urls.py
@@ -1,5 +1,7 @@
 """URL configuration for pyconde_talks project."""
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
@@ -10,3 +12,6 @@ urlpatterns = [
     path("accounts/", include("allauth.urls")),
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
