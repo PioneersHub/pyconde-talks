878b43f442481be0f7052e0ad57423f307d72c9b
Julio Batista Silva <julio@juliobs.com>
2025-02-18 04:14:28 +0100

    urls.py: add TalkListView at talks/

--- pyconde_talks/urls.py
+++ pyconde_talks/urls.py
@@ -6,10 +6,13 @@ from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
 
+from talks.views import TalkListView
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
+    path("talks/", TalkListView.as_view(), name="talk_list"),
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
 ]
 
