fc224f76bb54aa9a61fbc0bc5bf3f01ac39eb46b
Julio Batista Silva <julio@juliobs.com>
2025-02-18 03:43:47 +0100

    talks/admin.py: admin page for Talks

--- talks/admin.py
+++ talks/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Talk
+
+
+@admin.register(Talk)
+class TalkAdmin(admin.ModelAdmin):
+    list_display = ("title", "speaker_name", "date_time", "room")
+    list_filter = ("room", "date_time")
+    search_fields = ("title", "speaker_name", "description")
+    date_hierarchy = "date_time"
+    ordering = ("date_time",)
+
+    fieldsets = (
+        (
+            "Basic Information",
+            {
+                "fields": ("title", "speaker_name", "description"),
+            },
+        ),
+        (
+            "Schedule",
+            {
+                "fields": ("date_time", "room"),
+            },
+        ),
+        (
+            "Links",
+            {
+                "fields": ("pretalx_link", "video_link"),
+            },
+        ),
+        (
+            "Media",
+            {
+                "fields": ("image",),
+            },
+        ),
+    )
