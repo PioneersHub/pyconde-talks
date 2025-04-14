4166bd85b9438da5e384cd4ac7d218e110b3d833
Julio Batista Silva <julio@juliobs.com>
2025-04-14 23:00:15 +0200

    settings.py: CSRF_COOKIE_SECURE

--- pyconde_talks/settings.py
+++ pyconde_talks/settings.py
@@ -105,6 +105,8 @@ SESSION_COOKIE_HTTPONLY = True
 SESSION_COOKIE_SECURE = env.bool("SESSION_COOKIE_SECURE", True)
 # https://docs.djangoproject.com/en/dev/ref/settings/#csrf-cookie-httponly
 CSRF_COOKIE_HTTPONLY = True
+# https://docs.djangoproject.com/en/dev/ref/settings/#csrf-cookie-secure
+CSRF_COOKIE_SECURE = env.bool("CSRF_COOKIE_SECURE", True)
 # https://docs.djangoproject.com/en/dev/ref/settings/#x-frame-options
 X_FRAME_OPTIONS = "DENY"
 
