ad95e866a22fdbf8ce7dbe6ef0dfae3b8251721c
Luiz Filipe <moresco.luiz@gmail.com>
2025-05-29 17:14:45 +0200

    Improve docstring

--- utils/url.py
+++ utils/url.py
@@ -2,7 +2,7 @@ from furl import furl
 
 
 def add_query_param(url: str, key: str, value: str) -> str:
-    """Enrich URL add new query params and return the new url."""
+    """Enrich the URL adding a new query param and return the new url."""
     f = furl(url)
     f.add({key: value})
     return f.url
