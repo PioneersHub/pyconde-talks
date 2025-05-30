361a6bf60b4a939ce041bf89d197c4ec270727d9
Luiz Filipe <moresco.luiz@gmail.com>
2025-05-30 11:18:28 +0200

    Replace Enum by StrEnum

--- talks/validators.py
+++ talks/validators.py
@@ -3,7 +3,7 @@ from talks.types import VideoProvider
 
 
 def validate_video_link(video_link: str):
-    is_valid_video_provider = any(provider.value in video_link for provider in VideoProvider)
+    is_valid_video_provider = any(provider in video_link for provider in VideoProvider)
 
     if not is_valid_video_provider:
         video_provider_names = [x.name for x in VideoProvider]
