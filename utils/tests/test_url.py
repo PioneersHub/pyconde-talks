90763a0938f38082ed5313c4434673be20ee99d1
Luiz Filipe <moresco.luiz@gmail.com>
2025-05-30 12:43:26 +0200

    Implement utils.url unit tests

---
+++ utils/tests/test_url.py
@@ -0,0 +1,23 @@
+"""
+Conference talk management module for PyCon DE & PyData 2025.
+
+This module provides unit test for utils.url.
+"""
+
+from utils.url import add_query_param
+
+
+class TestURL:
+    """TestURL implements unit tests for all utils.url."""
+
+    def test_add_query_param_change_url(self) -> None:
+        """Test if add_query_param adds a new query param accordingly."""
+        url = "http://abc.com"
+
+        new_url = add_query_param(url, "new_param", "new_value")
+
+        assert "?new_param=new_value" in new_url
+
+        new_url = add_query_param(new_url, "new_param_2", "new_value_2")
+
+        assert "&new_param_2=new_value_2" in new_url
