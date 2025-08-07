a82b8315112e4ed4c8d1e10ef0b64d1df1c88fd1
Theodore Meynard <theodore.meynard@Theodores-MacBook-Pro.local>
2025-08-07 17:50:19 +0200

    change back manage.py

--- manage.py
+++ manage.py
@@ -4,14 +4,12 @@
 import os
 import sys
 
-from django.core.management import execute_from_command_line
-
 
 def main() -> None:
     """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pyconde_talks.settings")
     try:
-        execute_from_command_line(sys.argv)
+        from django.core.management import execute_from_command_line
     except ImportError as exc:
         msg = (
             "Couldn't import Django. Are you sure it's installed and "
@@ -21,6 +19,7 @@ def main() -> None:
         raise ImportError(
             msg,
         ) from exc
+    execute_from_command_line(sys.argv)
 
 
 if __name__ == "__main__":
