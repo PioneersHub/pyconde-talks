db6bb02d1bfc8bd4d8c4275e5c281be684d7b059
Theodore Meynard <theodore.meynard@Theodores-MacBook-Pro.local>
2025-08-12 22:49:58 +0200

    make question accepted by default

--- pyconde_talks/urls.py
+++ pyconde_talks/urls.py
@@ -18,7 +18,6 @@ from talks.views_qa import (
     QuestionCreateView,
     QuestionListView,
     QuestionUpdateView,
-    approve_question,
     delete_question,
     mark_question_answered,
     reject_question,
@@ -56,7 +55,6 @@ urlpatterns = [
     path("questions/<int:question_id>/vote/", vote_question, name="question_vote"),
     path("questions/<int:question_id>/edit/", QuestionUpdateView.as_view(), name="question_edit"),
     path("questions/<int:question_id>/delete/", delete_question, name="question_delete"),
-    path("questions/<int:question_id>/approve/", approve_question, name="question_approve"),
     path("questions/<int:question_id>/reject/", reject_question, name="question_reject"),
     path(
         "questions/<int:question_id>/mark-answered/",
